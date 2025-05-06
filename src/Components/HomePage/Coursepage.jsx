d2a8885534545602f1989bcc477ef24c572a04c4
Sumit-Singh94 <sumitsin3122@gmail.com>
2025-05-06 23:10:41 +0530

    updated code

--- src/Components/HomePage/Coursepage.jsx
+++ src/Components/HomePage/Coursepage.jsx
@@ -5,12 +5,6 @@ import { useParams } from 'react-router-dom'
 function Coursepage() {
   const { coursecode } = useParams()
   
-  useEffect(() => {
-    console.log("Coursepage component mounted with coursecode:", coursecode);
-  }, [coursecode]);
-
-  console.log("Coursepage render called with coursecode:", coursecode);
-
   return (
     <div>
       <h1 className='text-red-600'>this is a coursepage: {coursecode}</h1>
