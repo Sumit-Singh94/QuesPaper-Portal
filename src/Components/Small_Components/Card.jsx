8e655486beb6fbeac7ba04c0d04b8ad4f3725e86
Sumit-Singh94 <sumitsin3122@gmail.com>
2025-05-07 23:16:57 +0530

    updated code

--- src/Components/Small_Components/Card.jsx
+++ src/Components/Small_Components/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Courses from '../../Courses';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 
 
 export const Card = ({course}) => {
--- src/main.jsx
+++ src/main.jsx
@@ -19,7 +19,7 @@ const router = createBrowserRouter([
     element: <HomeScreen />
    },
    {
-    path: "course/:coursecode",
+    path: "/course/:coursecode",
     element: <Coursepage />
    }
   ]
