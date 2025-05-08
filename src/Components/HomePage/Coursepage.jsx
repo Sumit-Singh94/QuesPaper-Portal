fd78191ee35c93a18a1cdaefef776b3a0c13f09a
Sumit-Singh94 <sumitsin3122@gmail.com>
2025-05-08 19:54:33 +0530

    updated coursepage

--- src/Components/HomePage/Coursepage.jsx
+++ src/Components/HomePage/Coursepage.jsx
@@ -1,7 +1,7 @@
 // Updated Coursepage.jsx with debugging
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import { Courses } from '..'
+
 
 export function Coursepage() {
 
